1d735b37d411873b21b692d1411935e531226ae2
Konstantin <zakharov83@mail.ru>
2025-04-27 20:52:43 +0300

    added some functionality and provided comments

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 
+// STEP 1. CREATE THREE CONSTANTS
+//         AND ASSIGN VALUES TO THEM. TO ACCOMPLISH THIS
+//         USE THE BUILT-IN DOCUMENT.GETELE,ENTBYID METHOD
 
 const decrease = document.getElementById("decrease");
 const reset = document.getElementById('reset');
 const increase = document.getElementById('increase');
 const label = document.getElementById('count');
+
+// STEP 2. CREATE A VARIABLE THAT WILL HOLD THE VALUES
+//         OF THE LABEL
+
 let count = 0;
 
+// STEP 3. CREATE THREE FUNCTIONS TO INCREASE, DECREASE, AND RESET
+//         THE COUNT.
+
 decrease.onclick = function() {
+    count--;
+    label.textContent = count;
+}
+
+increase.onclick = () => {
     count++;
+    label.innerHTML = count;
+}
+
+reset.onclick = () => {
+    count = 0;
     label.textContent = count;
 }
